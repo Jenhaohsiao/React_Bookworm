608730143b868f1870588fe1b4130dfa9793106c
JenHao <jenhao.hsiao@gmail.com>
2018-04-04 19:39:53 -0400

    Added data pass function

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import {Link}from "react-router-dom";
+import LoginForm from "../forms/LoginForm";
 
-const LoginPage = () => (
+class LoginPage extends React.Component {
 
-    <div>
-        <h1>Login Page</h1>
-         
-    </div>
-);
+    submit = data => {
+        console.log(data);
+    };
+    render(){
+        return(
+        <div>
+            <h1>Login Page</h1>     
+             <LoginForm submit={this.submit} />
+        </div>
+        );
+    }
+}
 
 export default LoginPage;
