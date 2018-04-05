4f1b6fe94abe510433ab6fb4d45cfdbdcc0072d6
JenHao <jenhao.hsiao@gmail.com>
2018-04-05 15:35:49 -0400

    setup redux

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import {Link}from "react-router-dom";
 
-const HomePage = () => (
+// const HomePage = () => (
 
+//     <div>
+//         <h1>Home Page</h1>
+//         <div><Link to ="/login">Login</Link></div>
+//         <div><Link to ="/about">About</Link></div>
+        
+//     </div>
+// );
+
+class HomePage extends React.Component {
+    render(){
+        return(
     <div>
         <h1>Home Page</h1>
-        <Link to ="/login">Login</Link>
+        <div><Link to ="/login">Login</Link></div>
+        <div><Link to ="/about">About</Link></div>
     </div>
-);
+        );
+    }
+}
 
 export default HomePage;
